11f3fd3b1f47fec98f28578c9d5f1d1b3940aa67
Ichlasul al <m.ichal7749@mail.unpas.ac.id>
2019-12-13 08:22:27 +0700

    membetulkan tampilan product

--- application/views/Home/index.php
+++ application/views/Home/index.php
@@ -125,7 +125,7 @@
 				<!-- /section title -->
 
 				<!-- Products tab & slick -->
-				<div class="col-md-12">
+				<div class="col-3 my-3">
 					<div class="row">
 						<div class="products-tabs">
 							<!-- tab -->
@@ -162,12 +162,28 @@
 										</div>
 									</div>
 									<!-- /product -->
+								</div>
+								<div id="slick-nav-1" class="products-slick-nav"></div>
+							</div>
+							<!-- /tab -->
+						</div>
+					</div>
+				</div>
+				<!-- Products tab & slick -->
 
+				<!-- Products tab & slick -->
+				<div class="col-3 my-3">
+					<div class="row">
+						<div class="products-tabs">
+							<!-- tab -->
+							<div id="tab1" class="tab-pane active">
+								<div class="products-slick" data-nav="#slick-nav-1">
 									<!-- product -->
 									<div class="product">
 										<div class="product-img">
-											<img src="<?= base_url('assets/') ?>/img/electro/product02.png" alt="">
+											<img src="<?= base_url('assets/') ?>/img/electro/product01.png" alt="">
 											<div class="product-label">
+												<span class="sale">-30%</span>
 												<span class="new">NEW</span>
 											</div>
 										</div>
@@ -180,7 +196,7 @@
 												<i class="fa fa-star"></i>
 												<i class="fa fa-star"></i>
 												<i class="fa fa-star"></i>
-												<i class="fa fa-star-o"></i>
+												<i class="fa fa-star"></i>
 											</div>
 											<div class="product-btns">
 												<button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
@@ -193,37 +209,30 @@
 										</div>
 									</div>
 									<!-- /product -->
+								</div>
+								<div id="slick-nav-1" class="products-slick-nav"></div>
+							</div>
+							<!-- /tab -->
+						</div>
+					</div>
+				</div>
+				<!-- Products tab & slick -->
 
+				<!-- Products tab & slick -->
+				<div class="col-3 my-3">
+					<div class="row">
+						<div class="products-tabs">
+							<!-- tab -->
+							<div id="tab1" class="tab-pane active">
+								<div class="products-slick" data-nav="#slick-nav-1">
 									<!-- product -->
 									<div class="product">
 										<div class="product-img">
-											<img src="<?= base_url('assets/') ?>/img/electro/product03.png" alt="">
+											<img src="<?= base_url('assets/') ?>/img/electro/product01.png" alt="">
 											<div class="product-label">
 												<span class="sale">-30%</span>
+												<span class="new">NEW</span>
 											</div>
-										</div>
-										<div class="product-body">
-											<p class="product-category">Category</p>
-											<h3 class="product-name"><a href="#">product name goes here</a></h3>
-											<h4 class="product-price">$980.00 <del class="product-old-price">$990.00</del></h4>
-											<div class="product-rating">
-											</div>
-											<div class="product-btns">
-												<button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
-												<button class="add-to-compare"><i class="fa fa-exchange"></i><span class="tooltipp">add to compare</span></button>
-												<button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
-											</div>
-										</div>
-										<div class="add-to-cart">
-											<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
-										</div>
-									</div>
-									<!-- /product -->
-
-									<!-- product -->
-									<div class="product">
-										<div class="product-img">
-											<img src="<?= base_url('assets/') ?>/img/electro/product04.png" alt="">
 										</div>
 										<div class="product-body">
 											<p class="product-category">Category</p>
@@ -247,11 +256,77 @@
 										</div>
 									</div>
 									<!-- /product -->
+								</div>
+								<div id="slick-nav-1" class="products-slick-nav"></div>
+							</div>
+							<!-- /tab -->
+						</div>
+					</div>
+				</div>
+				<!-- Products tab & slick -->
 
+				<!-- Products tab & slick -->
+				<div class="col-3 my-3">
+					<div class="row">
+						<div class="products-tabs">
+							<!-- tab -->
+							<div id="tab1" class="tab-pane active">
+								<div class="products-slick" data-nav="#slick-nav-1">
 									<!-- product -->
 									<div class="product">
 										<div class="product-img">
-											<img src="<?= base_url('assets/') ?>/img/electro/product05.png" alt="">
+											<img src="<?= base_url('assets/') ?>/img/electro/product01.png" alt="">
+											<div class="product-label">
+												<span class="sale">-30%</span>
+												<span class="new">NEW</span>
+											</div>
+										</div>
+										<div class="product-body">
+											<p class="product-category">Category</p>
+											<h3 class="product-name"><a href="#">product name goes here</a></h3>
+											<h4 class="product-price">$980.00 <del class="product-old-price">$990.00</del></h4>
+											<div class="product-rating">
+												<i class="fa fa-star"></i>
+												<i class="fa fa-star"></i>
+												<i class="fa fa-star"></i>
+												<i class="fa fa-star"></i>
+												<i class="fa fa-star"></i>
+											</div>
+											<div class="product-btns">
+												<button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
+												<button class="add-to-compare"><i class="fa fa-exchange"></i><span class="tooltipp">add to compare</span></button>
+												<button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
+											</div>
+										</div>
+										<div class="add-to-cart">
+											<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+										</div>
+									</div>
+									<!-- /product -->
+								</div>
+								<div id="slick-nav-1" class="products-slick-nav"></div>
+							</div>
+							<!-- /tab -->
+						</div>
+					</div>
+				</div>
+				<!-- Products tab & slick -->
+
+				<!-- Products tab & slick -->
+				<div class="col-3 my-3">
+					<div class="row">
+						<div class="products-tabs">
+							<!-- tab -->
+							<div id="tab1" class="tab-pane active">
+								<div class="products-slick" data-nav="#slick-nav-1">
+									<!-- product -->
+									<div class="product">
+										<div class="product-img">
+											<img src="<?= base_url('assets/') ?>/img/electro/product01.png" alt="">
+											<div class="product-label">
+												<span class="sale">-30%</span>
+												<span class="new">NEW</span>
+											</div>
 										</div>
 										<div class="product-body">
 											<p class="product-category">Category</p>
@@ -284,8 +359,11 @@
 				</div>
 				<!-- Products tab & slick -->
 			</div>
+
 			<!-- /row -->
+
 		</div>
 		<!-- /container -->
+
 	</div>
 	<!-- /SECTION -->
