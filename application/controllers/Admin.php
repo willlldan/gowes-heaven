eac91f9591325fdb3f0c2fa110032c0cd0746108
adam <kilmeisdisaster@gmail.com>
2019-12-21 17:53:22 +0700

    user & admin masih banyak yg eror

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,18 +1,18 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class User extends CI_Controller
+class Admin extends CI_Controller
 {
 	public function index() 
 	{
-		$data['title'] = 'My Profile';
+		$data['title'] = 'Dashboard';
 		$data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
 		$data['user']['name'];
 
 		$this->load->view('templates/user_header', $data);
 		$this->load->view('templates/user_sidebar', $data);
 		$this->load->view('templates/user_topbar', $data);
-		$this->load->view('user/index', $data);
+		$this->load->view('admin/index', $data);
 		$this->load->view('templates/footer');
 	}
 }
--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -43,7 +43,12 @@ class Auth extends CI_Controller
                         'role_id' => $user['role_id']
                     ];
                     $this->session->set_userdata($data);
-                    redirect('home');
+                   if ($user['role_id'] == 1) {
+                    redirect('admin');
+
+                   }  else{
+                        redirect('user');
+                   }
                 } else {
                     $this->session->set_flashdata('message','<div class="alert alert-danger" role="alert">Wrong password!</div>');
             redirect('auth');
