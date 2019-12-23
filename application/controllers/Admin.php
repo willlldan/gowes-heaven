bf3a3a2d459c34630102ce84490c813f78674527
Wildan Fauzi Rakhman <173040071@mail.unpas.ac.id>
2019-12-23 17:48:53 +0700

    Mulai membuat panel admin

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -3,7 +3,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Admin extends CI_Controller
 {
-	public function index() 
+	public function index()
 	{
 		$data['title'] = 'Dashboard';
 		$data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
@@ -13,6 +13,6 @@ class Admin extends CI_Controller
 		$this->load->view('templates/user_sidebar', $data);
 		$this->load->view('templates/user_topbar', $data);
 		$this->load->view('admin/index', $data);
-		$this->load->view('templates/footer');
+		$this->load->view('templates/user_footer');
 	}
 }
--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,16 +1,61 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
 
-  
-        <!-- Begin Page Content -->
-        <div class="container-fluid">
+  <!-- Page Heading -->
+  <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
-          <!-- Page Heading -->
-          <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
-
-
+  <!-- start jumlah pesanan -->
+  <div class="row">
+    <!-- card pesanan bulan ini -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-left-primary shadow h-100 py-2">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Pesanan bulan ini</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">4</div>
+            </div>
+            <div class="col-auto">
+              <i class="fas fa-calendar fa-2x text-gray-300"></i>
+            </div>
+          </div>
         </div>
-        <!-- /.container-fluid -->
-
       </div>
-      <!-- End of Main Content -->
+    </div>
+    <!-- end card bulan ini -->
+    <!-- start pesanan pending -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-left-info shadow h-100 py-2">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Pesanan pending</div>
+              <div class="row no-gutters align-items-center">
+                <div class="col-auto">
+                  <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                </div>
+                <div class="col">
+                  <div class="progress progress-sm mr-2">
+                    <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-auto">
+              <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- end pesanan pending -->
+  </div>
 
-      
+
+  <!-- end jumlah pesanan -->
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
