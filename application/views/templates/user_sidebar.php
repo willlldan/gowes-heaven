3b4f988762b3fb2476edfea844494d0cee4467f9
Lukman hakim <lukman_hakim@mail.unpas.ac.id>
2019-12-26 16:29:40 +0700

    coba benerin link logiut sama tampil gambar di user

--- application/views/templates/user_sidebar.php
+++ application/views/templates/user_sidebar.php
@@ -4,33 +4,19 @@
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
       <!-- Sidebar - Brand -->
-      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?=base_url('home/')?>">
         <div class="sidebar-brand-icon rotate-n-15">
          <i class="fas fa-bicycle"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">Gowes Admin <sup></sup></div>
+        <div class="sidebar-brand-text mx-3">Gowes User <sup></sup></div>
       </a>
 
       <!-- Divider -->
       <hr class="sidebar-divider">
-      <!-- Heading -->
-      <div class="sidebar-heading">
-        Administrator
-      </div>
-
-      <!-- Nav Item - Dashboard -->
-      <li class="nav-item">
-        <a class="nav-link" href="index.html">
-          <i class="fas fa-fw fa-tachometer-alt"></i>
-          <span>Dashboard</span></a>
-      </li>
-
-      <!-- Divider -->
-      <hr class="sidebar-divider">
 
       <!-- Heading -->
       <div class="sidebar-heading">
-        User
+        <?=$user['name']?>
       </div>
 
 
